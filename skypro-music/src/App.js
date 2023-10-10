d340d807de9e8e4e93c67d1d3fba6586f388992e
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2023-10-10 20:36:35 +0300

    Проект на react

---
+++ skypro-music/src/App.js
@@ -0,0 +1,12 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <code>Здесь скоро будет Skypro.Music</code>
+    </div>
+  );
+}
+
+export default App;
