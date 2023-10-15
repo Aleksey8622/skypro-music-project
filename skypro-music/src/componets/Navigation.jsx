e331078e9411940844be114914532cf329948fc5
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2023-10-15 17:01:24 +0300

    Первый пункт дз-3 сделал

--- skypro-music/src/componets/Navigation.jsx
+++ skypro-music/src/componets/Navigation.jsx
@@ -1,19 +1,26 @@
 import "../App.css";
 import BurgerMenu from "./BurgerMenu";
+import React from "react";
+const { useState } = React;
 function Navigation() {
+  const [show, setShow] = useState(true);
+
+  const showMenu = () => setShow(!show);
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
         <img className="logo__image" src="img/icon/logo.svg" alt="logo" />
       </div>
-      <div className="nav__burger burger">
+      <div className="nav__burger burger" onClick={showMenu}>
         <span className="burger__line"></span>
         <span className="burger__line"></span>
         <span className="burger__line"></span>
       </div>
-      <div className="nav__menu menu">
-        <BurgerMenu />
-      </div>
+      {show && (
+        <div className="nav__menu menu">
+          <BurgerMenu />
+        </div>
+      )}
     </nav>
   );
 }
